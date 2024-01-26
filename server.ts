4389a240329813b74fad9902893eaedc3d881d44
Adi Hai <adihai27@gmail.com>
2024-01-26 10:23:39 +0200

    Change the server to return the files

--- server.ts
+++ server.ts
@@ -1,26 +1,33 @@
-var http = require('http');
+// var http = require('http');
 
-const hostName = '127.0.0.1';
-const port = 3000;
+// const hostName = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-    if (req.url == '/'){
-        res.writeHead(200, {'content-type':'text/plain' }) ;
-        res.write('This is home Page');
-        res.end();
-    }
-    else if (req.url == "/student"){
-        res.writeHead(200, { 'content-type':'text/plain' });
-        res.write('This is Student Page');
-        res.end();
-    }
-    else if (req.url == "/admin"){
-        res.writeHead(200, { 'content-type':'text/plain' });
-        res.write('This is Admin Page');
-        res.end();
-    }
-    else res.end('Invalid Request!');
+// const server = http.createServer((req, res) => {
+//     if (req.url == '/'){
+//         res.writeHead(200, {'content-type':'text/plain' }) ;
+//         res.write('This is home Page');
+//         res.end();
+//     }
+//     else if (req.url == "/student"){
+//         res.writeHead(200, { 'content-type':'text/plain' });
+//         res.write('This is Student Page');
+//         res.end();
+//     }
+//     else if (req.url == "/admin"){
+//         res.writeHead(200, { 'content-type':'text/plain' });
+//         res.write('This is Admin Page');
+//         res.end();
+//     }
+//     else res.end('Invalid Request!');
+// });
+
+// server.listen(port);
+// console.log('Noode.js web server at prt ${port} is running');
+
+const testFolder = './';
+const fs = require('fs');
+
+fs.readdirSync(testFolder).forEach(file => {
+  console.log(file);
 });
-
-server.listen(port);
-console.log('Noode.js web server at prt ${port} is running');
